39b58d68dbf5f4f6e09a490cd83afa3e7ee41bf9
Oscar Linderoth <oscar.linderoth@sportamore.se>
2016-09-29 09:49:47 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from setuptools import setup
+
+setup(
+    name='sportamore_toolbox',
+    packages=['toolbox'],
+    description='Sportamore Toolbox',
+    url='https://github.com/Sportamore/sportamore_toolbox',
+    version='0.1',
+    author='Oscar Linderoth',
+    install_requires=[
+        'django >= 1.6.8',
+    ],
+)
+
