17bfe7bb674c915133b9a3f09ce9c76199402bf1
Carl Skeide <carl@skeide.se>
2018-02-08 17:05:51 +0100

    Fix package manifest

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # coding: utf-8
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sportamore_toolbox',
-    packages=['toolbox'],
+    packages=find_packages(),
     description='Sportamore Toolbox',
     url='https://github.com/Sportamore/sportamore_toolbox',
     version='0.1',
@@ -14,4 +14,3 @@ setup(
         'furl >= 0.5.6',
     ],
 )
-
