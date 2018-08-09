969fc57d1dae69b7590b46e21e6ede688dd55cf1
Carl Skeide <carl.skeide@sportamore.se>
2018-08-09 10:47:32 +0200

    move monkey-print helper to extensions

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ setup(
     name='sportamore_toolbox',
     packages=find_packages(),
     description='Sportamore Toolbox',
-    url='https://github.com/Sportamore/sportamore_toolbox',
-    version='1.1.0',
+    url='https://github.com/Sportamore/toolbox',
+    version='2.0.1',
     author='Oscar Linderoth',
     install_requires=[
-        'django >= 1.6.8',
         'pytz >= 2015.6',
         'furl >= 0.5.6',
     ],
--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -21,4 +21,3 @@ __all__ = [
     'suppress_logging',
     'unpack',
 ]
-
