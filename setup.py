5beb09dc7fbe5a09d7809a868dcd647847bb72c5
ShmakovVA <v@shmakov.me>
2018-10-23 15:47:12 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 # coding: utf-8
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='toolbox',
-    packages=find_packages(),
+    packages='toolbox',
     description='Sportamore Toolbox',
     url='https://github.com/ShmakovVA/toolbox',
     author='No',
+    version=2.1.1,
     install_requires=[
         'pytz >= 2015.6',
         'furl >= 0.5.6',
@@ -14,13 +15,5 @@ setup(
     include_package_data=True,
     extras_require={
         ':python_version == "2.7"': ['futures']
-    },
-    setup_requires=[
-        'vcversioner'
-    ],
-    vcversioner={
-        'version_module_paths': [
-            'toolbox/_vcs.py'
-        ],
     },
 )
