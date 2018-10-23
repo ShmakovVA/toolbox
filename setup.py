f749a02e52cd682eec295e286d5779bf0370cad9
ShmakovVA <v@shmakov.me>
2018-10-23 14:53:56 +0500

    include_package_data=True,

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ setup(
     name='sportamore_toolbox',
     packages=find_packages(),
     description='Sportamore Toolbox',
-    url='https://github.com/Sportamore/toolbox',
-    author='Oscar Linderoth',
+    url='https://github.com/ShmakovVA/toolbox',
+    author='No',
     install_requires=[
         'pytz >= 2015.6',
         'furl >= 0.5.6',
     ],
+    include_package_data=True,
     extras_require={
         ':python_version == "2.7"': ['futures']
     },
